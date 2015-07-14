0148faa3b8a4db5f18f15f1147ff7c77151c1b52
carlosmz87 <carlosmartz1995@gmail.com>
2015-07-14 15:38:23 -0600

    primer envio

---
+++ hoja1/src/radio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author carlosmartinez
+ */
+public interface radio {
+    
+     public void setEstado(boolean Estado);
+    
+    public boolean getEstado();                          
+    
+    public int getAMFM();
+    
+    public void setAMFM(int banda);
+    
+    public void sintonizar(boolean ud);
+    
+    public void guardar(int pos);
+    
+    public void memoria(int pos);
+    
+    public float getEmisora();
+    
+}
