53d4bf5f8f3ea5c50cdbf1802f7c581576c13a35
carlosmz87 <carlosmartz1995@gmail.com>
2015-07-15 00:07:18 -0600

    tercer envio

--- hoja1/src/driver.java
+++ hoja1/src/driver.java
@@ -8,63 +8,121 @@
  *
  * @author carlosmartinez
  */
-public class driver implements radio{
-private int banda, pos, emisora=540;
-private boolean Estado, ud;
+import java.util.Scanner;
+public class driver{
+private static Scanner teclado; 
+private static int opciones;
+private static int AmFm=0;
+private static int memoria;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-         Stack<Integer> miStack = new StackArrayList<Integer>();
-         
-         
-    }
-
-    @Override
-    public void setEstado(boolean Estado) { 
-    this.Estado=Estado;
-    }
-
-    @Override
-    public boolean getEstado() { 
-    return Estado;
-    }
-
-    @Override
-    public int getAMFM() {
-    return banda;  
+    procesador radio = new procesador();
+    teclado=new Scanner(System.in);
+    System.out.print("Ingrese el numero de boton que desea presionar");
+    opciones=teclado.nextInt();
     
-    }
-
-    @Override
-    public void setAMFM(int banda) {
-    this.banda=banda;
+    while(opciones!=8){
+            
+        if(opciones==1){
+            radio.setEstado(true);
+            if(radio.getEstado()==true){
+                System.out.println("El radio esta encendido");
+                System.out.print("Ingrese el numero de boton que desea presionar");
+                opciones=teclado.nextInt();
     
-    }
-
-    @Override
-    public void sintonizar(boolean ud) {
+            }
+        }
         
+        else if(opciones==2){
+        radio.setEstado(false);
+            if(radio.getEstado()==false){
+                System.out.println("El radio se apagara en este momento");
+                System.exit(0);
+            }
+        
+        }
+        
+        else if(opciones==3){
+            radio.setAMFM(0);
+            if(radio.getAMFM()==0){
+                System.out.println("El radio esta en AM");
+                System.out.print("Ingrese el numero de boton que desea presionar");
     
+                opciones=teclado.nextInt();
+    
+            }
+        }
+        
+        else if(opciones==4){
+        radio.setAMFM(1);
+            if(radio.getAMFM()==1){
+                System.out.println("El radio esta en FM");
+                System.out.print("Ingrese el numero de boton que desea presionar");
+    
+                opciones=teclado.nextInt();
+    
+            }
+    
+        }
+        
+        
+        else if(opciones==5){
+            if(radio.getAMFM()==0){
+                System.out.println("El radio esta sintonizando en AM");
+                radio.sintonizar(false);
+                System.out.println(radio.getEmisora());
+                System.out.print("Ingrese el numero de boton que desea presionar");
+    
+                opciones=teclado.nextInt();
+    
+            }
+        
+            if(radio.getAMFM()==1){
+                System.out.println("El radio esta sintonizando en FM");
+                radio.sintonizar(true);
+                System.out.println(radio.getEmisora());
+                System.out.print("Ingrese el numero de boton que desea presionar");
+    
+                opciones=teclado.nextInt();
+    
+            }
+    
+        }
+        
+        
+        else if(opciones==6){
+        System.out.println("Seleccione el numero de memoria que desea utilizar 1-12");
+        memoria=teclado.nextInt();
+        radio.guardar(memoria);
+        System.out.print("Ingrese el numero de boton que desea presionar");
+    
+        opciones=teclado.nextInt();
+    
+        
+        }
+        
+        else if(opciones==7){
+        System.out.println("Seleccione el numero de memoria que desea verificar 1-12");
+        memoria=teclado.nextInt();
+        radio.memoria(memoria);
+        System.out.print("Ingrese el numero de boton que desea presionar");
+    
+        opciones=teclado.nextInt();
+    
+        
+        }
+        
+        else{
+            System.out.println("Ingrese una opcion correcta");
+            System.out.print("Ingrese el numero de boton que desea presionar");
+    
+            opciones=teclado.nextInt();
+    
+        }
+        
     }
-
-    @Override
-    public void guardar(int pos) {
-       
-    
     }
-
-    @Override
-    public void memoria(int pos) {
-    
-    
-    }
-
-    @Override
-    public double getEmisora() {
-    return emisora;   
-    
-    }
-    
 }
