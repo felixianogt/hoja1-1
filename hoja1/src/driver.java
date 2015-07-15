871d0d00c57e2ce1a3e3f24b66f21e15c0e78a6b
carlosmz87 <carlosmartz1995@gmail.com>
2015-07-14 18:11:57 -0600

    segundo envio

--- hoja1/src/driver.java
+++ hoja1/src/driver.java
@@ -8,13 +8,63 @@
  *
  * @author carlosmartinez
  */
-public class driver {
-
+public class driver implements radio{
+private int banda, pos, emisora=540;
+private boolean Estado, ud;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         Stack<Integer> miStack = new StackArrayList<Integer>();
+         
+         
+    }
+
+    @Override
+    public void setEstado(boolean Estado) { 
+    this.Estado=Estado;
+    }
+
+    @Override
+    public boolean getEstado() { 
+    return Estado;
+    }
+
+    @Override
+    public int getAMFM() {
+    return banda;  
+    
+    }
+
+    @Override
+    public void setAMFM(int banda) {
+    this.banda=banda;
+    
+    }
+
+    @Override
+    public void sintonizar(boolean ud) {
+        
+    
+    }
+
+    @Override
+    public void guardar(int pos) {
+       
+    
+    }
+
+    @Override
+    public void memoria(int pos) {
+    
+    
+    }
+
+    @Override
+    public double getEmisora() {
+    return emisora;   
+    
     }
     
 }
