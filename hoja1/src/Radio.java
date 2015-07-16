0f7263c5b4111db8b955987e4708a178233f0a3c
felixianogt <felix9514@gmail.com>
2015-07-15 20:13:13 -0600

    Update Radio.java

--- hoja1/src/Radio.java
+++ hoja1/src/Radio.java
@@ -1,3 +1,10 @@
+/**
+ * Carlos Martinez
+ * Diego Contreras
+ * Diego felix
+ */
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
