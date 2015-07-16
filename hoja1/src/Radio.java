66055b53bbc8e4e776025d4c055f19bcd4f0fbf8
felixianogt <felix9514@gmail.com>
2015-07-15 20:00:49 -0600

    Update Radio.java

--- hoja1/src/Radio.java
+++ hoja1/src/Radio.java
@@ -16,20 +16,29 @@
  */
 public interface Radio {
     
-     public void setESTADO(boolean Estado);
+    //se definen los metodos a implementarse en la interfaz principal
+    //verifica si el radio esta encendido u apagado
+    public void setESTADO(boolean Estado);
     
+    //recibe el estado actual
     public boolean getEstado();                          
     
+    //recibe a banda seleccionada
     public int getAMFM();
     
+    //se instancia la banda deseada
     public void setAMFM(int banda);
     
+    //sintonizara una emisora en especifica dentro de un rango de valores
     public void Sintonizar(boolean ud);
     
+    //guardara la estacion deseada en memoria
     public void Guardar(int pos);
     
+    //espacios disponibles para el almacenamiento de estaciones
     public void Memoria(int pos);
     
+    //muestra la emisora sintonizada
     public double getEmisora();
     
 }
